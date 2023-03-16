2a18cc0c4d185360b8d3c74eaf1eb21247750210
Michiel van der Meer <contact@michielvandermeer.com>
2023-03-16 17:02:57 +0100

    Removed GuardClass NuGet package from dependencies

--- mvdmio.ValueConversion.UnitsOfMeasurement.Tests/Utils/AssertExtensions.cs
+++ mvdmio.ValueConversion.UnitsOfMeasurement.Tests/Utils/AssertExtensions.cs
@@ -1,4 +1,3 @@
-using Ardalis.GuardClauses;
 using mvdmio.ValueConversion.Base.Utils;
 using Xunit;
 
@@ -6,17 +5,18 @@ namespace mvdmio.ValueConversion.UnitsOfMeasurement.Tests.Utils;
 
 public static class AssertExtensions
 {
-    public static void AreWithinTolerance(double expected, double actual, double tolerance = Comparer.DefaultTolerance)
-    {
-        var diff = Math.Abs(expected - actual);
-        Assert.True(diff < tolerance, $"Expected {expected} to be within {tolerance} of {actual}, but was {diff}.");
-    }
+   public static void AreWithinTolerance(double expected, double actual, double tolerance = Comparer.DefaultTolerance)
+   {
+      var diff = Math.Abs(expected - actual);
+      Assert.True(diff < tolerance, $"Expected {expected} to be within {tolerance} of {actual}, but was {diff}.");
+   }
 
-    public static void AreWithinPercentTolerance(double expected, double actual, double toleranceFraction = 1E-5)
-    {
-        Guard.Against.Zero(expected, nameof(expected));
+   public static void AreWithinPercentTolerance(double expected, double actual, double toleranceFraction = 1E-5)
+   {
+      if (Math.Abs(expected) < double.Epsilon)
+         throw new ArgumentException("Must not be zero", nameof(expected));
 
-        var diffPercent = Math.Abs((expected - actual) / expected);
-        Assert.True(diffPercent < toleranceFraction, $"Expected {expected} to be within {toleranceFraction}% of {actual}, but was {diffPercent}%");
-    }
+      var diffPercent = Math.Abs((expected - actual) / expected);
+      Assert.True(diffPercent < toleranceFraction, $"Expected {expected} to be within {toleranceFraction}% of {actual}, but was {diffPercent}%");
+   }
 }
